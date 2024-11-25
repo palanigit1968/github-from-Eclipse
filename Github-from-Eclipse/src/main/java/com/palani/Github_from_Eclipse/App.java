8032f4ab812765f3f6ddf7440e6ea74ab862efb9
Palani <palselvam@gmail.com>
2024-11-25 14:59:15 +0530

    FIRST COMMIT

---
+++ Github-from-Eclipse/src/main/java/com/palani/Github_from_Eclipse/App.java
@@ -0,0 +1,13 @@
+package com.palani.Github_from_Eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
